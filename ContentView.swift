9861cc71689a2dbbb342022d42236ad59c971eff
andrei-pliusnin <167740728+andrei-pliusnin@users.noreply.github.com>
2025-08-05 14:21:40 +0900

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,13 +1,11 @@
 import SwiftUI
 
-// MARK: - ContentView
 struct ContentView: View {
     var body: some View {
         EnhancedLoginView()
     }
 }
 
-// MARK: - Preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
